6cea9de25c49a3735eb73afeda92e55083f3a859
supermay <wu_jiamei@outlook.com>
2017-08-16 09:59:18 +0200

    Other user sign_up disabled

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root "articles#index"
+  devise_scope :user do
+    get "/users/sign_up", :to => "articles#index"
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
