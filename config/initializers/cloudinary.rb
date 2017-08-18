13160046afc31b74395ea9d389b14dc42d026512
lorenzograndi4 <lorenzo.grandi4@gmail.com>
2017-08-18 15:12:43 +0200

    new try for cloudinary production setup

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,26 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'lorenzocloudinary'
+  config.api_key = '486865665656777'
+  config.api_secret = 'j05QhesrqbQWDwjqKXmbGRx5FU8'
+  config.cdn_subdomain = true
+end
+
+
+# development:
+#   cloud_name: lorenzocloudinary
+#   api_key: '486865665656777'
+#   api_secret: j05QhesrqbQWDwjqKXmbGRx5FU8
+#   enhance_image_tag: true
+#   static_image_support: false
+# production:
+#   cloud_name: lorenzocloudinary
+#   api_key: '486865665656777'
+#   api_secret: j05QhesrqbQWDwjqKXmbGRx5FU8
+#   enhance_image_tag: true
+#   static_image_support: true
+# test:
+#   cloud_name: lorenzocloudinary
+#   api_key: '486865665656777'
+#   api_secret: j05QhesrqbQWDwjqKXmbGRx5FU8
+#   enhance_image_tag: true
+#   static_image_support: false
